570745bffc431b6b27a64d157011218523ad8403
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-05-20 10:57:17 -0500

    prepare.py

--- prepare.py
+++ prepare.py
@@ -1,66 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
+# import splitting and imputing functions
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
-# ------------------- IRIS DATA -------------------
-
-def clean_iris(df):
-
-    '''Prepares acquired Iris data for exploration'''
-    
-    # drop column using .drop(columns=column_name)
-    df = df.drop(columns='species_id')
-    
-    # remame column using .rename(columns={current_column_name : replacement_column_name})
-    df = df.rename(columns={'species_name':'species'})
-    
-    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
-    dummy_df = pd.get_dummies(df['species'], drop_first=False)
-    
-    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
-    df = pd.concat([df, dummy_df], axis=1)
-    
-    return df
-
-
-def split_iris_data(df):
-    '''
-    take in a DataFrame and return train, validate, and test DataFrames; stratify on species.
-    return train, validate, test DataFrames.
-    '''
-    
-    # splits df into train_validate and test using train_test_split() stratifying on species to get an even mix of each species
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.species)
-    
-    # splits train_validate into train and validate using train_test_split() stratifying on species to get an even mix of each species
-    train, validate = train_test_split(train_validate, 
-                                       test_size=.3, 
-                                       random_state=123, 
-                                       stratify=train_validate.species)
-    return train, validate, test
-
-
-def prep_iris(df):
-    '''Prepares acquired Iris data for exploration'''
-    
-    # drop column using .drop(columns=column_name)
-    df = df.drop(columns='species_id')
-    
-    # remame column using .rename(columns={current_column_name : replacement_column_name})
-    df = df.rename(columns={'species_name':'species'})
-    
-    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
-    dummy_df = pd.get_dummies(df['species'], drop_first=False)
-    
-    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
-    df = pd.concat([df, dummy_df], axis=1)
-    
-    # split data into train/validate/test using split_data function
-    train, validate, test = split_iris_data(df)
-    
-    return train, validate, test
+# turn off pink boxes for demo
+import warnings
+warnings.filterwarnings("ignore")
 
 # ------------------- TITANIC DATA -------------------
 
@@ -137,7 +86,7 @@ def prep_titanic_data(df):
     
     train, validate, test = impute_mean_age(train, validate, test)
 
-    return
+    return train, validate, test
 
 # ------------------- TELCO DATA -------------------
 
